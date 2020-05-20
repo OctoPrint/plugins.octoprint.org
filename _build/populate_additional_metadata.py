7f958d8a05d80d1e49f677b344e3d389216983df
Gina Häußge <gina@octoprint.org>
2020-05-20 18:42:15 +0200

    Adjusted for new stats format

--- _build/populate_additional_metadata.py
+++ _build/populate_additional_metadata.py
@@ -11,17 +11,27 @@ from datetime import datetime
 
 GITHUB_CREDENTIALS = os.environ.get("GITHUB_CREDENTIALS", None)
 
-PLUGINSTATS_URL = "https://data.octoprint.org/export/plugin_stats.json"
+PLUGINSTATS_7D_URL = "https://data.octoprint.org/export/plugin_stats_7d.json"
+PLUGINSTATS_30D_URL = "https://data.octoprint.org/export/plugin_stats_30d.json"
+
 GITHUBREPO_URL = "https://api.github.com/repos/{user}/{repo}"
 
-_plugin_stats = None
+_plugin_stats_7d = None
+_plugin_stats_30d = None
 
-def plugin_stats(plugin):
-	global _plugin_stats
-	if _plugin_stats is None:
-		resp = requests.get(PLUGINSTATS_URL)
-		_plugin_stats = resp.json()
-	return _plugin_stats.get(plugin)
+def plugin_stats_7d(plugin):
+	global _plugin_stats_7d
+	if _plugin_stats_7d is None:
+		resp = requests.get(PLUGINSTATS_7D_URL)
+		_plugin_stats_7d = resp.json()
+	return _plugin_stats_7d.get(plugin)
+
+def plugin_stats_30d(plugin):
+	global _plugin_stats_30d
+	if _plugin_stats_30d is None:
+		resp = requests.get(PLUGINSTATS_30D_URL)
+		_plugin_stats_30d = resp.json()
+	return _plugin_stats_30d.get(plugin)
 
 def github_data(user, repo):
 	if GITHUB_CREDENTIALS is not None:
@@ -46,18 +56,30 @@ def process_plugin_file(path, incl_stats=True, incl_github=True):
 	print("Processing plugin {} at path {}".format(plugin_id, path))
 
 	if incl_stats:
-		stats = plugin_stats(data["id"].lower())
-		if stats is not None:
-			print("Enriching {} with stats...".format(plugin_id))
-			data["stats"] = dict(installations=stats["instances"],
-			                     install_events=stats.get("install_events"))
+		def build_stats(stats):
+			result = dict(instances=stats.get("instances", 0),
+			              install_events=stats.get("install_events", 0))
 			if "versions" in stats:
-				data["stats"]["versions"] = dict()
+				result["versions"] = dict()
 				for version in stats["versions"]:
-					data["stats"]["versions"][version] = stats["versions"][version]["instances"]
-		else:
-			data["stats"] = dict(installations=0,
-			                     install_events=0)
+					result["versions"][version] = stats["versions"][version]["instances"]
+
+			return result
+
+		data["stats"] = {"week": dict(instances=0,
+		                              install_events=0),
+		                 "month": dict(instances=0,
+		                               install_events=0)}
+
+		stats7d = plugin_stats_7d(data["id"].lower())
+		if stats7d is not None:
+			print("Enriching {} with stats for week...".format(plugin_id))
+			data["stats"]["week"] = build_stats(stats7d)
+
+		stats30d = plugin_stats_30d(data["id"].lower())
+		if stats30d is not None:
+			print("Enriching {} with stats for month...".format(plugin_id))
+			data["stats"]["month"] = build_stats(stats30d)
 
 	if incl_github:
 		user = repo = None
