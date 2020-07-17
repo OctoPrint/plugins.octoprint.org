9cc7a69da4c8739d231ce7eb0f4d08ccbbaf36cc
Gina Häußge <gina@octoprint.org>
2020-07-17 12:48:22 +0200

    Move to .github and add action workflow output

--- .github/scripts/validate_front_matter.py
+++ .github/scripts/validate_front_matter.py
@@ -39,7 +39,7 @@ def ImageLocation(v):
 		if not v.startswith("/assets/img/plugins/"):
 			raise Invalid("image location '{}' must either be an URL or a path starting with /assets/img/plugins/".format(v))
 
-		image_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", v[1:]))
+		image_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", v[1:]))
 		if not os.path.exists(image_path):
 			raise Invalid("image location '{}' doesn't exist on disk ({})".format(v, image_path))
 
@@ -84,10 +84,38 @@ SCHEMA = Schema({
 
 def validate_schema(data):
 	SCHEMA(data)
+	return []
+
+def validate_image_urls(data, path):
+	warnings = []
+
+	filename = os.path.basename(path)[:-3]
+	def check_url(loc, url):
+		try:
+			Url(url)
+		except Invalid:
+			return
+		else:
+			message = "image is hosted externally, should be moved to /assets/img/plugins/{} @ {}".format(filename, loc)
+			warnings.append(message)
+
+	if "screenshots" in data:
+		count = 0
+		for entry in data["screenshots"]:
+			check_url("data['screenshots'][{}]['url']".format(count), entry["url"])
+			count += 1
+
+	if "featuredimage" in data:
+		check_url("data['featuredimage']", data["featuredimage"])
+
+	return warnings
 
 def validate_id_match(data, path):
-	if data["id"] != os.path.basename(path)[:-3]:
-		raise ValueError("id {} for {} does not match file name".format(data["id"], path))
+	filename = os.path.basename(path)[:-3]
+	if data["id"] != filename:
+		raise ValueError("id '{}' does not match file name '{}.md'".format(data["id"], filename))
+
+	return []
 
 def validate_date_unchanged(data, path, cwd, sha):
 	gitpath = path[len(cwd) + 1:]
@@ -107,56 +135,88 @@ def validate_date_unchanged(data, path, cwd, sha):
 	if data["date"] != old_metadata.get("date"):
 		raise ValueError("date must not be changed after initial registration")
 
-def validate(cwd, path, id_match=False, date_unchanged=False):
+	return []
+
+def validate(cwd, path, id_match=False, internal_assets=False, date_unchanged=False):
 	with codecs.open(path, mode="r", encoding="utf-8") as f:
 		metadata, content = frontmatter.parse(f.read())
-	SCHEMA(metadata)
+
+	warnings = []
+
+	warnings += validate_schema(metadata)
 
 	if id_match:
-		validate_id_match(metadata, path)
+		warnings += validate_id_match(metadata, path)
+
+	if internal_assets:
+		warnings += validate_image_urls(metadata, path)
 
 	if date_unchanged and path.startswith(cwd):
-		validate_date_unchanged(metadata, path, cwd, date_unchanged)
+		warnings += validate_date_unchanged(metadata, path, cwd, date_unchanged)
+
+	return warnings
 
 @click.command()
 @click.option("--debug", is_flag=True)
-@click.option("--id-match", "id_match", is_flag=True)
-@click.option("--date-unchanged", "date_unchanged", help="Provide git committish with which to compare")
+@click.option("--check-id-match", "id_match", is_flag=True)
+@click.option("--check-internal-assets", "internal_assets", is_flag=True)
+@click.option("--check-date-unchanged", "date_unchanged", help="Provide git committish with which to compare")
+@click.option("--action-output", "action_output", is_flag=True)
 @click.argument("paths", nargs=-1)
-def main(paths, debug, id_match, date_unchanged):
+def main(paths, debug=False, id_match=False, internal_assets=False, date_unchanged=None, action_output=False):
 	count = 0
 	fails = 0
 
-	paths = list(map(os.path.abspath, paths))
+	cwd = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+	plugin_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "_plugins"))
 
-	cwd = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
-	plugin_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "_plugins"))
-	with os.scandir(plugin_dir) as it:
-		for entry in it:
-			if not entry.is_file() or not entry.name.endswith(".md"):
-				continue
+	if not len(paths):
+		paths = []
+		with os.scandir(plugin_dir) as it:
+			for entry in it:
+				if not entry.is_file() or not entry.name.endswith(".md"):
+					continue
+				paths.append(entry.path)
 
-			path = entry.path
-			if paths and path not in paths:
-				continue
+	for path in paths:
+		try:
+			warnings = validate(cwd, path,
+			                    id_match=id_match,
+			                    internal_assets=internal_assets,
+			                    date_unchanged=date_unchanged)
+		except Exception as exc:
+			print("{}: ".format(path), end="")
+			print(colorama.Fore.RED + colorama.Style.BRIGHT + "FAIL")
 
-			try:
-				validate(cwd, path,
-				         id_match=id_match,
-				         date_unchanged=date_unchanged)
-			except Exception as exc:
-				print("{}: ".format(path), end="")
-				print(colorama.Fore.RED + colorama.Style.BRIGHT + "FAIL")
-				print("  " + str(exc))
-				fails += 1
+			if action_output:
+				print("::error file={}::{}".format(path[len(cwd) + 1:], str(exc)))
 			else:
-				if debug:
-					print("{}: ".format(path), end="")
+				print("  " + str(exc))
+
+			fails += 1
+		else:
+			if debug or len(warnings):
+				print("{}: ".format(path), end="")
+				if len(warnings):
+					print(colorama.Fore.YELLOW + colorama.Style.BRIGHT + "WARN")
+
+					for warning in warnings:
+						if action_output:
+							print("::warning file={}::{}".format(path[len(cwd) + 1:], warning))
+						else:
+							print("  " + warning)
+
+				else:
 					print(colorama.Fore.GREEN + colorama.Style.BRIGHT + "PASS")
 
-			count += 1
+		count += 1
 
 	print("Validated {} files, {} passes, {} fails".format(count, count - fails, fails))
+	if action_output:
+		print("::set-output name=files::{}".format(count))
+		print("::set-output name=passes::{}".format(count - fails))
+		print("::set-output name=fails::{}".format(fails))
+
 	if fails != 0:
 		sys.exit(-1)
 
